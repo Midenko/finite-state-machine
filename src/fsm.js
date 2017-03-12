d193518438f34855db98bd9f1d2bc832c89c54be
Midenko <kostyagrand@mail.ru>
2017-03-13 00:15:22 +0200

    Add files via upload

--- src/fsm.js
+++ src/fsm.js
@@ -1,48 +1,67 @@
 class FSM {
 
-    constructor(config) {
-        this.state = config.initial;
-        this.states = config.states;
-    }    
+	constructor(config) {
+		this.state = config.initial;
+		this.states = config.states;
+		this.history = [config.initial];
+	}
 
-    getState() {
-        return this.state;
-    }
-
-
-    changeState(state) {
-        this.state = state;
-    }
+	getState() {
+		return this.state;
+	}
 
 
-    trigger(event) {
-        this.state = this.states[this.state]['transitions'][event];
-    }
+	changeState(state) {
+		this.history.push(this.state);
+		this.state = state;
+	}
 
 
-    reset() {
-        this.state = config.initial;
-    }
+	trigger(event) {
+		this.state = this.states[this.state]['transitions'][event];
+		this.history.push(this.state);
+	}
 
 
-    getStates(event) {
-        var result = [];
-        for (var key in this.states) {
-            //if (key['transitions'] === event) {
-                result.push(key['transitions']);
-            //}
-        }
-        return result;
-    }
-
-    undo() {}
+	reset() {
+		this.state = config.initial;
+	}
 
 
-    redo() {}
+	getStates(event) {
+		var result = [];
+		for (var key in this.states) {
+			var arrOfStates = Object.keys(this.states[key]['transitions']);
+			arrOfStates.forEach(function (item) {
+				if (item === event) result.push(key);
+			});
+		}
+		return result;
+	}
 
-    clearHistory() {}
+	undo() {
+		if (this.state === config.initial) return false;
+		if (this.history.length) {
+			this.state = this.history[this.history.length - 2];
+			return true;
+		}
+		return false;
+	}
+
+
+	redo() {
+		if (this.history.length > 1) {
+			this.state = this.history[1]
+			this.history = this.history.slice(1, 1);
+			return true;
+		}
+		return false;
+	}
+
+	clearHistory() {
+		this.state = config.initial;
+		this.history = [config.initial];
+	}
 }
 
 module.exports = FSM;
-
-
